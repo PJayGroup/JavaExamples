85adb89098f0fdcacc495a22946142a4f9577e07
vijay konduru <konduruvijaykumar@gmail.com>
2016-05-15 23:08:32 +0530

    Vijay - 2016/05/15 - Adding example code on reentrant locks

---
+++ Concurrency/src/org/pjaygroup/concurrency/reentrantlock/ReentrantTrylockApp.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.pjaygroup.concurrency.reentrantlock;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+/**
+ * @author Vijay
+ *
+ */
+public class ReentrantTrylockApp {
+	
+	private static final String CLASS_NAME = ReentrantTrylockApp.class.getName();
+	private static final Logger LOGGER = Logger.getLogger(CLASS_NAME);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LOGGER.entering(CLASS_NAME, "entering main()");
+		ExecutorService FisherManExtSrv = Executors.newSingleThreadExecutor();
+		ExecutorService SharkExtSrv = Executors.newSingleThreadExecutor();
+		FisherManExtSrv.submit(new FisherManThread());
+		SharkExtSrv.submit(new SharkThread());
+		FisherManExtSrv.shutdown();
+		SharkExtSrv.shutdown();
+		LOGGER.exiting(CLASS_NAME, "exiting main()");
+	}
+
+}
