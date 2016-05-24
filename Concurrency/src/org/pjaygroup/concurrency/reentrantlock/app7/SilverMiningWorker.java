3048b3ac0eeb3866509059683e80f0c076c3ffc3
vijay konduru <konduruvijaykumar@gmail.com>
2016-05-24 05:49:05 +0530

    Vijay - 2016/05/24 - Adding some more examples to concurrency

---
+++ Concurrency/src/org/pjaygroup/concurrency/reentrantlock/app7/SilverMiningWorker.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.pjaygroup.concurrency.reentrantlock.app7;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Vijay
+ *
+ */
+public class SilverMiningWorker implements Runnable{
+	
+	private static final Logger LOGGER = Logger.getLogger(SilverMiningWorker.class.getName());
+
+	@Override
+	public void run() {
+		while(true){
+			MetalVault.getInstance().getReentrantLock().lock();
+			try{
+				if(MetalVault.getInstance().getSilverBars().get() <= 0){
+					if(MetalVault.getInstance().getSilverBars().get() < 0){break;}
+					try {
+						MetalVault.getInstance().getSilverCondition().await();
+						LOGGER.info(" :: Waking up SilverMiningWorker :: ");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}else{
+					LOGGER.info("SilverMiningWorker Taking " + MetalVault.getInstance().getSilverBars().get() + " Silver bars");
+					MetalVault.getInstance().setSilverBars(0);
+				}
+			} finally {
+				MetalVault.getInstance().getReentrantLock().unlock();
+			}
+		}
+	}
+
+}
